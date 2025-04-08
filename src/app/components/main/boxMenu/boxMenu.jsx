f5706c6b0c3eb4caadb452cdee88ec8f80d12e1c
MW-Limah <cdrmw16@gmail.com>
2025-04-08 23:40:55 +0100

    Trabalhando em ajustes no itembox

--- src/app/components/main/boxMenu/boxMenu.jsx
+++ src/app/components/main/boxMenu/boxMenu.jsx
@@ -1,3 +1,17 @@
+import styles from './boxMenu.module.css';
+
 export default function BoxMenu() {
-    return <div></div>;
+    return (
+        <article className={styles.boxContent}>
+            <div className={styles.boxMenutitle}>
+                <h2>Make</h2>
+            </div>
+            <div className={styles.boxMenu}>
+                <div className={styles.boxItem}>1</div>
+                <div className={styles.boxItem}>1</div>
+                <div className={styles.boxItem}>1</div>
+                <div className={styles.boxItem}>1</div>
+            </div>
+        </article>
+    );
 }
--- src/app/components/main/main.jsx
+++ src/app/components/main/main.jsx
@@ -1,9 +1,16 @@
 import styles from './main.module.css';
+import BoxMenu from './boxMenu/boxMenu';
 
 export default function Main() {
     return (
         <main className={styles.Main}>
-            <div className={styles.boxContent}>Conteudo</div> {/* Div que irá receber o boxItem */}
+            <div className={styles.boxContent}>
+                <BoxMenu />
+                <BoxMenu />
+                <BoxMenu />
+                <BoxMenu />
+            </div>
+            {/* Div que irá receber o boxItem */}
             <div className={styles.lineContent}></div> {/* Div que irá receber o lineItem */}
         </main>
     );

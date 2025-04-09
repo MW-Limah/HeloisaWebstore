73a7cfbc3c1f686ec1519ea62c6529db4dc274aa
MW-Limah <cdrmw16@gmail.com>
2025-04-09 13:40:55 +0100

    Favicon

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,24 +1,32 @@
 import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
 
+// Fontes
 const geistSans = Geist({
-    variable: '--font-geist-sans',
     subsets: ['latin'],
+    variable: '--font-geist-sans',
+    display: 'swap',
 });
 
 const geistMono = Geist_Mono({
-    variable: '--font-geist-mono',
     subsets: ['latin'],
+    variable: '--font-geist-mono',
+    display: 'swap',
 });
 
+// Metadados da página
 export const metadata = {
     title: 'Heloisa Moda Feminina',
-    description: 'Loja virtual, ecommerce, Heloisa Moda Feminina, moda feminina, acessorios para mulheres',
+    description: 'Loja virtual, ecommerce, Heloisa Moda Feminina, moda feminina, acessórios para mulheres',
+    icons: {
+        icon: '/favicon.png',
+    },
 };
 
+// Layout principal
 export default function RootLayout({ children }) {
     return (
-        <html lang="en">
+        <html lang="pt-BR">
             <body className={`${geistSans.variable} ${geistMono.variable}`}>{children}</body>
         </html>
     );

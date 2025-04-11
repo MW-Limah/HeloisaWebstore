3f26c8ffd6671a7ed3190b471dc9948ce97cc13d
MW-Limah <cdrmw16@gmail.com>
2025-04-11 21:59:20 +0100

    Form
    Adicionei um css no form, mas apenas nos botões, agora falta criar o "subform" que será responsável por criar e gerenciar os itens no banco de dados

--- src/app/pages/Admin/form/form.jsx
+++ src/app/pages/Admin/form/form.jsx
@@ -3,24 +3,36 @@ import styles from './form.module.css';
 export default function Form() {
     return (
         <div className={styles.container}>
-            <div className={styles.LeftSide}>
-                <h2>O que você quer fazer?</h2>
-                <ul>
-                    <li>
-                        <h4>Caixa de Items</h4>
-                        <button>{'->'}</button>
-                    </li>
-                    <li>
-                        <h4>Linha de Items</h4>
-                        <button>{'->'}</button>
-                    </li>
-                    <li>
-                        <h4>Slide</h4>
-                        <button>{'->'}</button>
-                    </li>
-                </ul>
+            <div className={styles.topSide}>
+                <h1>O que você deseja fazer?</h1>
             </div>
-            <div className={styles.RightSide}></div>
+            <div className={styles.downSide}>
+                <div className={styles.leftSide}>
+                    <ul className={styles.ul1}>
+                        <li>
+                            <h4>Caixa de Items</h4>
+                        </li>
+                        <li>
+                            <h4>Linha de Items</h4>
+                        </li>
+                        <li>
+                            <h4>Editar Slides</h4>
+                        </li>
+                    </ul>
+                    <ul className={styles.ul2}>
+                        <li>
+                            <button>➔</button>
+                        </li>
+                        <li>
+                            <button>➔</button>
+                        </li>
+                        <li>
+                            <button>➔</button>
+                        </li>
+                    </ul>
+                </div>
+                <div className={styles.rightSide}></div>
+            </div>
         </div>
     );
 }

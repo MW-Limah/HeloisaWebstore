5922a76ea6dc466953afe1223b7bb49ecadd4a2d
MW-Limah <cdrmw16@gmail.com>
2025-04-13 21:18:48 +0100

    CSS

--- src/app/components/main/main.jsx
+++ src/app/components/main/main.jsx
@@ -8,13 +8,9 @@ export default function Main() {
             <div className={styles.boxContent}>
                 <BoxMenu />
             </div>
-            {/* Div que irá receber o boxItem */}
+
             <div className={styles.lineContent}>
-                {/* <LineMenu id="Brincos" /> */}
                 <LineMenu id="Piranhas" />
-                {/* <LineMenu id="Maquiagens" />
-                <LineMenu id="Pulseiras" />
-                <LineMenu id="Aneis" /> */}
             </div>
             <h3 style={{ color: 'black', position: 'relative', top: '-180px' }}>Toque na tulipaaa</h3>
             {/* Div que irá receber o lineItem */}

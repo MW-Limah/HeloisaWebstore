9a8cb09ca5e0ecc5c78692eb7806084f1192e6da
MW-Limah <cdrmw16@gmail.com>
2025-04-18 22:56:20 +0100

    Navbar ajustes

--- src/app/components/navbar/navbar.jsx
+++ src/app/components/navbar/navbar.jsx
@@ -16,7 +16,7 @@ export default function Navbar() {
 
     useEffect(() => {
         const closeMenu = (e) => {
-            if (isActive && !e.target.closest(`.${styles.navbarContent}`)) {
+            if (isActive && !e.target.closest(`.${styles.gp2}`) && !e.target.closest(`.${styles.Bars}`)) {
                 setIsActive(false);
             }
         };
@@ -31,46 +31,48 @@ export default function Navbar() {
                 <div className={styles.leftContent}>
                     <Image src={'/logo_1.png'} width={100} height={100} alt="Logo Principal Heloisa Moda Feminina" />
                 </div>
+
+                {/* Visível apenas em telas maiores que 1100px */}
                 <div className={styles.rightContent}>
                     <Link href={'#'}>Olá, inscreva-se para atualizações</Link>
                     <Link href={'#'}>Entre em contato!</Link>
                     <Link href="/pages/Login">Acessar área Admin</Link>
                 </div>
-            </div>
-            <div className={styles.bottomContent}>
-                <ul>
-                    <div className={styles.gp1}>
-                        <li className={styles.Bars} onClick={toggleMenu}>
-                            {isActive ? (
-                                <IoCloseSharp style={{ color: 'white' }} />
-                            ) : (
-                                <FaBars style={{ color: 'white' }} />
-                            )}
-                        </li>
-                        <li>
-                            <Link href={'#Brincos'}>Brincos</Link>
-                        </li>
-                        <li>
-                            <Link href={'#Piranhas'}>Piranhas</Link>
-                        </li>
-                    </div>
-                    <div className={`${styles.gp2} ${isActive ? styles.active : ''}`}>
-                        <div className={styles.linksMenu}>
-                            <li>
-                                <Link href={'#Maquiagens'}>Maquiagem</Link>
-                            </li>
 
-                            <li>
-                                <Link href={'#Pulseiras'}>Pulseiras</Link>
-                            </li>
-                            <li>
-                                <Link href={'#Aneis'}>Anéis</Link>
-                            </li>
-                        </div>
-                        <div className={styles.Admin}>
-                            <Link href="/pages/Login">Acessar área Admin</Link>
-                        </div>
-                    </div>
+                {/* Ícone do menu para telas menores */}
+                <div className={styles.Bars} onClick={toggleMenu}>
+                    {isActive ? <IoCloseSharp /> : <FaBars />}
+                </div>
+            </div>
+
+            <div className={`${styles.gp2} ${isActive ? styles.active : ''}`}>
+                <ul className={styles.linksMenu}>
+                    <li>
+                        <Link href={'#Brincos'}>Brincos</Link>
+                    </li>
+                    <li>
+                        <Link href={'#Piranhas'}>Piranhas</Link>
+                    </li>
+                    <li>
+                        <Link href={'#Maquiagens'}>Maquiagem</Link>
+                    </li>
+                    <li>
+                        <Link href={'#Pulseiras'}>Pulseiras</Link>
+                    </li>
+                    <li>
+                        <Link href={'#Aneis'}>Anéis</Link>
+                    </li>
+
+                    {/* Estes aparecem só em mobile */}
+                    <li className={styles.mobileOnly}>
+                        <Link href={'#'}>Olá, inscreva-se para atualizações</Link>
+                    </li>
+                    <li className={styles.mobileOnly}>
+                        <Link href={'#'}>Entre em contato!</Link>
+                    </li>
+                    <li className={`${styles.mobileOnly} ${styles.Admin}`}>
+                        <Link href="/pages/Login">Acessar área Admin</Link>
+                    </li>
                 </ul>
             </div>
         </nav>

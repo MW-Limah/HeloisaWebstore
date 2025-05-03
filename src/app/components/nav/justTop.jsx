e8305e3d71151703f1c30cb4adf0500b14d00b1b
MW-Limah <cdrmw16@gmail.com>
2025-05-03 17:34:06 +0100

    Vou começar a implementar a seção de pagamento via pix

--- src/app/components/nav/justTop.jsx
+++ src/app/components/nav/justTop.jsx
@@ -9,10 +9,19 @@ import Image from 'next/image';
 import styles from './justTop.module.css';
 import ButtonBack from '@/app/components/buttonBack/buttonBack';
 import Cart from '@/app/components/GoCart/GoCart';
+import { useRouter } from 'next/navigation';
 
 export default function JustTop() {
     const [isActive, setIsActive] = useState(false);
     const pathname = usePathname();
+    const router = useRouter();
+    const handleBack = () => {
+        if (window.history.length > 1) {
+            router.back();
+        } else {
+            router.push('/'); // fallback para home se não houver página anterior
+        }
+    };
     const toggleMenu = () => {
         setIsActive(!isActive);
     };
@@ -21,7 +30,14 @@ export default function JustTop() {
         <div className={styles.navbar}>
             <div className={styles.topContent}>
                 <div className={styles.leftContent}>
-                    <Image src={'/logo_1.webp'} width={100} height={100} alt="Logo Principal Heloisa Moda Feminina" />
+                    <button className={styles.back__} onClick={handleBack}>
+                        <Image
+                            src={'/logo_1.webp'}
+                            width={100}
+                            height={100}
+                            alt="Logo Principal Heloisa Moda Feminina"
+                        />
+                    </button>
                 </div>
                 <div className={styles.rightContent}>
                     <ButtonBack />

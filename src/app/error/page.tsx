a2528f1664779ce49b44507ccff482adc4c68ead
MW-Limah <cdrmw16@gmail.com>
2025-07-12 21:46:10 +0100

    Error page

--- src/app/error/page.tsx
+++ src/app/error/page.tsx
@@ -4,7 +4,7 @@ import ButtonBackAll from '../components/buttonBackAll/buttonBack';
 export default function ErrorPage() {
     return (
         <div className={styles.container}>
-            <h1 className={styles.title}>Ops, temos um erro nessa página.</h1>
+            <h1 className={styles.title}>Ops, temos um erro ou estamos atualizando essa página.</h1>
             <p className={styles.message}>Desculpe-nos, estamos trabalhando para resolver isso logo. 🫡</p>
             <div>
                 <p className={styles.bye}>Por favor, volte mais tarde.</p>
